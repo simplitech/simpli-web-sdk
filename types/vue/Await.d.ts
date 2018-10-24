c89f845636d9037f65598bb9f21db29ef0afde9c
Felipe Gibran Eleuterio Toledo <ft.gibran@gmail.com>
2018-10-24 10:05:30 -0300

    fix: Fixed minimun bug

--- types/vue/Await.d.ts
+++ types/vue/Await.d.ts
@@ -1,15 +1,8 @@
 import { Component as Comp, AsyncComponent as AsyncComp } from 'vue/types'
-import { Vue } from 'vue-property-decorator'
 export declare type CP = Comp<any, any, any, any> | AsyncComp<any, any, any, any>
 export interface Loader {
     [key: string]: CP
 }
-export declare enum View {
-    DEFAULT = 0,
-    LOADING = 1,
-    ERROR = 2
-}
-export declare const Event: object & Record<never, any> & Vue
 export declare class AwaitController {
     defaultTransition: string | null
     defaultSpinner: string | null
@@ -22,24 +15,4 @@ export declare class AwaitController {
     done(name?: string): void
     error(name?: string): void
     run(func: Function, name?: string, delay?: number): Promise<any>
-}
-export declare class Await extends Vue {
-    name?: string
-    effect?: string
-    spinner?: string
-    spinnerColor?: string
-    spinnerPadding?: string
-    spinnerScale?: number
-    transition: string | null
-    loader: string | null
-    color: string | null
-    padding: string | null
-    zoom: number | null
-    view: View
-    height?: number
-    readonly hasLoadingSlot: boolean
-    readonly hasErrorSlot: boolean
-    readonly minHeight: string
-    mounted(): void
-    beforeMount(): void
 }
--- types/vue/Modal.d.ts
+++ types/vue/Modal.d.ts
@@ -1,9 +1,3 @@
-import { Vue } from 'vue-property-decorator'
-export declare const Event: object & Record<never, any> & Vue
-export declare enum State {
-    HIDDEN = 0,
-    SHOWN = 1
-}
 export declare class ModalController {
     defaultBody: HTMLElement
     defaultTransition: string | null
@@ -14,25 +8,4 @@ export declare class ModalController {
     close(name?: string): void
     toggle(name?: string): void
     closeAll(): void
-}
-export declare class Modal extends Vue {
-    name?: string
-    title?: string
-    innerClass?: string
-    effect?: string
-    backgroundEffect?: string
-    closable?: boolean
-    closeOutside?: boolean
-    state: State
-    transition: string | null
-    backgroundTransition: string | null
-    isClosable: boolean
-    isCloseOutside: boolean
-    body: HTMLElement | null
-    bodyOverflowY: string | null
-    stateEvent(val: State): void
-    open(): void
-    close(force?: boolean): void
-    closeFromView(e: Event): void
-    beforeMount(): void
 }
